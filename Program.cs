7dea816911f9b8e0d0c62b7e7920e7494d916649
Steve Aldworth <s.aldworth@tvdsb.ca>
2022-02-24 10:25:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace First_CSharp_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double age;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            age = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{name}, next year you will be {age + 1}");
+
+        }
+    }
+}
